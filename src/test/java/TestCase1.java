45e809c19c8aa981339695b44c686cc01f35c01a
sadareddy <gouthami012.s@gmail.com>
2020-08-07 15:50:10 +0530

    First Commit

---
+++ src/test/java/TestCase1.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestCase1 {
+	
+	
+	public WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void testCase1() {
+		
+		driver.get("http://gmail.com");
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+	}
+}
